2f37cafe474290a3c2c976eefc88296eef8b4a8b
ryan.attard <ryan.attard@mcast.edu.mt>
2021-02-18 11:46:23 +0100

    Google Authentication - not checked yet because we need to setup a db context

--- Solution1/WebApplication1/Startup.cs
+++ Solution1/WebApplication1/Startup.cs
@@ -34,6 +34,19 @@ namespace WebApplication1
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
             services.AddRazorPages();
+
+            services.AddAuthentication()
+                   .AddGoogle(options =>
+                   {
+                       IConfigurationSection googleAuthNSection =
+                           Configuration.GetSection("Authentication:Google");
+
+                       options.ClientId = googleAuthNSection["ClientId"];
+                       options.ClientSecret = googleAuthNSection["ClientSecret"];
+                   });
+
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
