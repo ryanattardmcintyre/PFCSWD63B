dd2839cbee8a6d3fee85d71a03f256a7f4f5e208
ryan.attard <ryan.attard@mcast.edu.mt>
2021-03-11 11:50:33 +0100

    Started Firestore

---
+++ Solution1/WebApplication1/DataAccess/Repositories/FirestoreBlogRepository.cs
@@ -0,0 +1,52 @@
+﻿using Google.Cloud.Firestore;
+using Google.Cloud.Firestore.V1;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.DataAccess.Interfaces;
+using WebApplication1.Domain;
+
+namespace WebApplication1.DataAccess.Repositories
+{
+    public class FirestoreBlogRepository : IBlogsRepository
+    {
+        IConfiguration _config;
+        FirestoreDb db;
+        public FirestoreBlogRepository(IConfiguration config)
+        {   _config = config;
+            string projectId = _config.GetSection("ProjectId").Value;
+            db = FirestoreDb.Create(projectId);
+        }
+
+        public void DeleteBlog(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Blog GetBlog(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<Blog> GetBlogs()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Guid InsertBlog(Blog b)
+        {
+            var id = Guid.NewGuid();
+            DocumentReference docRef = db.Collection("blogs").Document();
+            Dictionary<string, object> blog = new Dictionary<string, object>
+            {
+                { "Id", id },
+                { "Url", b.Url },
+                { "Title", b.Title }
+            };
+            docRef.SetAsync(blog).Wait();
+            return id;
+        }
+    }
+}
